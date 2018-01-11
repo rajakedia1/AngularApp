1f6aae4f280d9ce91cc06d05b32e0ceb1e730171
Raja Kedia(miyagix) <rajakedia2222@gmail.com>
2018-01-11 17:55:10 +0530

    Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
   declarations: [
@@ -32,7 +35,8 @@ import { ContactComponent } from './contact/contact.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+      AppRoutingModule
   ],
   providers: [
       DishService
